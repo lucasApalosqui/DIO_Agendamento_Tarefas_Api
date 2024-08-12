5da9dd46a298e8ac2c244ee2b865e188dcd46ffc
LucasApalosqui <lucas@apalosqui@gmail.com>
2024-08-12 17:17:08 -0300

    :sparkles: feat: Tarefa model

---
+++ Entities/Tarefa.cs
@@ -0,0 +1,46 @@
+﻿using DioAgendamentoTarefasApi.Enums;
+
+namespace DioAgendamentoTarefasApi.Entities
+{
+    public class Tarefa
+    {
+        public Tarefa(string titulo, string descricao, DateTime date)
+        {
+            Id = Guid.NewGuid();
+            Titulo = titulo;
+            Descricao = descricao;
+            Date = date;
+            Status = Status.Pendente;
+        }
+        public Guid Id { get; }
+        public string Titulo { get; private set; }
+        public string Descricao { get; private set; }
+        public DateTime Date { get; private set; }
+        public Status Status { get; private set; }
+
+        public void AlterarInformacoes(string? titulo, string? descricao)
+        {
+            if (!string.IsNullOrEmpty(descricao))
+                Descricao = descricao;
+
+            if (!string.IsNullOrEmpty(titulo))
+                Titulo = titulo;
+        }
+
+        public void AumentarDias(int dias)
+        {
+            if(dias > 0)
+                Date = Date.AddDays(dias);
+        }
+
+        public void EnviarParaLixeira()
+        {
+            Status = Status.Cancelado;
+        }
+
+        public void ConcluirTarefa()
+        {
+            Status = Status.Concluido;
+        }
+    }
+}
---
+++ Enums/Status.cs
@@ -0,0 +1,9 @@
+﻿namespace DioAgendamentoTarefasApi.Enums
+{
+    public enum Status
+    {
+        Pendente = 0,
+        Concluido = 1,
+        Cancelado = 2
+    }
+}
